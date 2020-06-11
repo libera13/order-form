9f670e56d5ddc5baff4b6068897af3688798e3cc
libera13 <emillibera@gmail.com>
2020-06-11 19:56:28 +0200

    Updates

---
+++ precache-manifest.38c635611c48fd5bc518c5e33a24203d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af02483da4b992fb3b5c2bd97153f852",
+    "url": "/order-form/index.html"
+  },
+  {
+    "revision": "a2dff073b1629311f8a4",
+    "url": "/order-form/static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "44006207c7e137e11421",
+    "url": "/order-form/static/css/main.799e7390.chunk.css"
+  },
+  {
+    "revision": "a2dff073b1629311f8a4",
+    "url": "/order-form/static/js/2.0c3f4453.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/order-form/static/js/2.0c3f4453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44006207c7e137e11421",
+    "url": "/order-form/static/js/main.ce6a21b4.chunk.js"
+  },
+  {
+    "revision": "db3c4b33b1c8b13b52ae",
+    "url": "/order-form/static/js/runtime-main.3cf6d500.js"
+  }
+]);
